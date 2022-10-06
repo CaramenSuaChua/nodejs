a3f4a3bd9b1b9542488322e69ff734e17d6b5ec8
Ngo Dung <ngodungvb0304@gmail.com>
2022-10-06 14:41:27 +0700

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+
+const app = express()
+const users = [];
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
+app.use( bodyParse.urlencoded({extended:false }))
+app.get('/', (req, res, next) => {
+    res.render('index', {pageTitle:' Add User'})
+})
+
+app.get('/users', (req, res, next) => {
+    res.render('users', {pageTitle:' User', users: users})
+})
+
+app.post('/add-user', (req, res, next) => {
+    users.push({name: req.body.username})
+    res.redirect('/users')
+})
+
+app.listen(5000)
